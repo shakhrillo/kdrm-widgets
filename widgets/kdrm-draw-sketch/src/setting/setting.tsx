dc821365435c61654fd97d858e1b58caf43b72c8
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-05-02 18:32:05 +0500

    Refactor KDRM Draw Sketch widget settings for improved layout and reduced default size

--- widgets/kdrm-draw-sketch/src/setting/setting.tsx
+++ widgets/kdrm-draw-sketch/src/setting/setting.tsx
@@ -1,38 +1,32 @@
-/** @jsx jsx */
-import { React, jsx } from 'jimu-core'
+import { React } from 'jimu-core'
 import type { AllWidgetSettingProps } from 'jimu-for-builder'
-import {
-  MapWidgetSelector,
-  SettingSection,
-  SettingRow
-} from 'jimu-ui/advanced/setting-components'
+import { MapWidgetSelector, SettingSection, SettingRow } from 'jimu-ui/advanced/setting-components'
 import type { IMConfig } from '../config'
 
 export default function (props: AllWidgetSettingProps<IMConfig>) {
+  const onMapWidgetSelected = (useMapWidgetIds: string[]) => {
+    props.onSettingChange({
+      id: props.id,
+      useMapWidgetIds: useMapWidgetIds
+    })
+  }
 
-    const onMapWidgetSelected = (useMapWidgetIds: string[]) => {
-        props.onSettingChange({
-            id: props.id,
-            useMapWidgetIds: useMapWidgetIds
-        })
-    }
-
-    return (
-        <div>
-            <SettingSection
-                className="map-selector-section"
-                title={props.intl.formatMessage({
-                    id: 'mapWidgetLabel',
-                    defaultMessage: 'KDRM Draw Sketch'
-                })}
-            >
-                <SettingRow>
-                    <MapWidgetSelector
-                        onSelect={onMapWidgetSelected}
-                        useMapWidgetIds={props.useMapWidgetIds}
-                    />
-                </SettingRow>
-            </SettingSection>
-        </div>
-    )
+  return (
+    <div>
+      <SettingSection
+        className="map-selector-section"
+        title={props.intl.formatMessage({
+          id: 'mapWidgetLabel',
+          defaultMessage: 'KDRM Draw Sketch'
+        })}
+      >
+        <SettingRow>
+          <MapWidgetSelector
+            onSelect={onMapWidgetSelected}
+            useMapWidgetIds={props.useMapWidgetIds}
+          />
+        </SettingRow>
+      </SettingSection>
+    </div>
+  )
 }
