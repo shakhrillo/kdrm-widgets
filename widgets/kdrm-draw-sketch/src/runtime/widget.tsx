b9ca53bb27b1474270b4b66fbeb35b5f75d5df9a
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-05-02 11:00:53 +0500

    Add KDRM Draw Sketch widget with initial configuration and UI components

---
+++ widgets/kdrm-draw-sketch/src/runtime/widget.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { React, jsx } from 'jimu-core'
+import type { AllWidgetProps } from 'jimu-core';
+import type { IMConfig } from '../config'
+
+export default function (props: AllWidgetProps<IMConfig>) {
+    return (
+        <p>
+            Welcome to KDRM Draw Sketch Widget!
+        </p>
+    )
+}
