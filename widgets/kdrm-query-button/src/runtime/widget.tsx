b917c335092103822c7f51e39d3d3ffc62267fcd
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2025-05-04 15:21:05 +0500

    Remove unused query logic and clean up widget code for better readability

--- widgets/kdrm-query-button/src/runtime/widget.tsx
+++ widgets/kdrm-query-button/src/runtime/widget.tsx
@@ -5,12 +5,6 @@ const { useState, useEffect, useRef } = React
 export default function Widget(props: AllWidgetProps<unknown>) {
   const [jimuMapView, setJimuMapView] = useState<JimuMapView>(null);
   const [polygone, setPolygone] = useState<__esri.Polygon>(null);
-  const [query, setQuery] = useState<FeatureLayerQueryParams>({
-    where: `1=1`,
-    outFields: ['objectid', 'facilityid'],
-    pageSize: 2
-  })
-  const cityNameRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     console.log('polygone', polygone)
@@ -35,47 +29,20 @@ export default function Widget(props: AllWidgetProps<unknown>) {
         // QueryTask,
         query
       ]) => {
-
-        // const polygonGeometry = new Polygon({
-        //   spatialReference: SpatialReference.WebMercator,
-        //   rings: [
-        //     [[-118.821527, 34.1377], [-118.814497, 34.1377], [-118.814497, 34.1325], [-118.821527, 34.1325], [-118.821527, 34.1377]]
-        //   ]
-        // });
-  
-        // Define the query parameters
-        // const query = new Query();
-        // query.geometry = polygone;
-        // query.spatialRelationship = Query.SPATIAL_REL_INTERSECTS;  // Check if features intersect the polygon
-        // query.returnGeometry = true;  // Return the geometry of the results
-        // query.outFields = ["*"];  // Specify the fields you want to return
-  
-        // Define the layer you want to query
-        // const layerUrl = ds.url;
-        // const queryTask = new QueryTask({
-        //   url: layerUrl
-        // });
   
         // Execute the query
         query.executeQueryJSON(ds.url, {
-          // facilityid === 133402
-          // where: "facilityid = '133402'",
           geometry: polygone,
           spatialRelationship: "intersects",
           returnGeometry: false,
-          outFields: ['*'],
-          // spatialRelationship: 'intersects',
+          outFields: ['*']
         }).then(function (results) {
           const features = results.features;
           console.log("Found features:", features);
-          // Do something with the results (e.g., display them on the map)
         }).catch(function (error) {
           console.error("Query failed: ", error);
         });
       })
-
-
-      
     }
 
     runQuery()
@@ -105,12 +72,7 @@ export default function Widget(props: AllWidgetProps<unknown>) {
     
   };
 
-  useEffect(() => {
-    queryFunc()
-  }, [])
-
   const isDsConfigured = () => {
-    console.log('__', props.useDataSources)
     if (props.useDataSources &&
       props.useDataSources.length === 1 &&
       props.useDataSources[0].fields &&
@@ -120,41 +82,13 @@ export default function Widget(props: AllWidgetProps<unknown>) {
     return false
   }
 
-  const queryFunc = () => {
-    // if (!isDsConfigured()) {
-    //   return
-    // }
-    // const fieldName = props.useDataSources[0].fields[0]
-    // const w = cityNameRef.current && cityNameRef.current.value
-    //   ? `${fieldName} like '%${cityNameRef.current.value}%'`
-    //   : '1=1'
-    // setQuery({
-    //   where: w,
-    //   outFields: [fieldName],
-    //   pageSize: 10
-    // })
-  }
-
   const dataRender = (ds: DataSource, info: IMDataSourceInfo) => {
-    //createOutputDs(ds);
-    // const fName = props.useDataSources[0].fields[0]
     const fields = props.useDataSources[0].fields
     return <>
-      <div>
-        <input placeholder="Query value" ref={cityNameRef} />
-        <button onClick={queryFunc}>Query</button>
-      </div>
       <div>Query state: {info.status}</div>
-      <div>Count: {ds.count}</div>
 
       <div className="record-list" style={{ width: '100%', marginTop: '20px', height: 'calc(100% - 80px)', overflow: 'auto' }}>
         {
-          // ds && ds.getStatus() === DataSourceStatus.Loaded
-          //   ? ds.getRecords().map((r, i) => {
-          //     return <div key={i}>{r.getData()[fName]}</div>
-          //   })
-          //   : null
-
           ds && ds.getStatus() === DataSourceStatus.Loaded
             ? ds.getRecords().map((r, i) => {
               return <div key={i} style={{ border: '1px solid #ccc', padding: '10px', marginBottom: '10px' }}>
@@ -170,23 +104,6 @@ export default function Widget(props: AllWidgetProps<unknown>) {
       </div>
     </>
   }
-
-
-  // const createOutputDs = (useDs: DataSource) => {
-  //   if (!props.outputDataSources) {
-  //     return
-  //   }
-  //   const outputDsId = props.outputDataSources[0]
-  //   const dsManager = DataSourceManager.getInstance()
-  //   if (dsManager.getDataSource(outputDsId)) {
-  //     if (dsManager.getDataSource(outputDsId).getDataSourceJson().originDataSources[0].dataSourceId !== useDs.id) {
-  //       dsManager.destroyDataSource(outputDsId)
-  //     }
-  //   }
-  //   dsManager.createDataSource(outputDsId).then(ods => {
-  //     ods.setRecords(useDs.getRecords())
-  //   })
-  // }
 
   if (!isDsConfigured()) {
     return <h3>
@@ -212,8 +129,6 @@ export default function Widget(props: AllWidgetProps<unknown>) {
     <h3>
       This widget shows how to use a feature layer as a data source.
     </h3>
-
-    {query && <h4>Query: {JSON.stringify(query)}</h4>}
 
     <DataSourceComponent useDataSource={props.useDataSources[0]} widgetId={props.id} queryCount>
       {dataRender}
